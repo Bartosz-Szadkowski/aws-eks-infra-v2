9dcd7c5499f58673ca48b1cff8719319ab75645b
Bartosz Szadkowski <bartoszszadkowski@NC-bartoszszadkowski-P6DMXQ7WYV.local>
2024-09-11 21:47:52 +0200

    added public subnets and private subnets for eks

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,46 @@
-resource "aws_vpc" "vpc" {
-  cidr_block       = var.cidr_block
+####################
+## VPC
+####################
+
+resource "aws_vpc" "this" {
+  cidr_block       = var.vpc_cidr_block
   instance_tenancy = "default"
+
+  # These are set to true to allow worker nodes join the cluster 
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
   tags = {
     Name = "${var.tags["Environment"]}-vpc"
   }
 }
+
+####################
+## PUBLIC SUBNETS
+####################
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets_cidr_blocks)
+
+  vpc_id = aws_vpc.this.id 
+  cidr_block = var.public_subnets_cidr_blocks[count.index]
+
+  tags = {
+    Name = "${var.tags["Environment"]}-public-subnet-${count.index + 1}"
+  }
+}
+
+
+####################
+## PRIVATE SUBNETS EKS
+####################
+resource "aws_subnet" "private_eks" {
+  count = length(var.private_subnets_eks_cidr_blocks)
+
+  vpc_id = aws_vpc.this.id 
+  cidr_block = var.private_subnets_eks_cidr_blocks[count.index]
+
+  tags = {
+    Name = "${var.tags["Environment"]}-private-subnet-${count.index + 1}"
+  }
+}
+
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,4 +1,12 @@
-variable "cidr_block" {}
+variable "vpc_cidr_block" {}
+
+variable "public_subnets_cidr_blocks" {
+  type = list(string)
+}
+
+variable "private_subnets_eks_cidr_blocks" {
+ type = list(string) 
+}
 
 variable "tags" {
   type        = map(string)
