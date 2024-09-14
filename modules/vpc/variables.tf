b3cff9eb1290c3717fda3ce609bc931a5116829f
Bartosz Szadkowski <bartoszszadkowski@NC-bartoszszadkowski-P6DMXQ7WYV.local>
2024-09-14 12:43:06 +0200

    vpc module created

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,3 +1,12 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "availability_zones" {
+ type = list(string) 
+ default = ["us-east-1a", "us-east-1b"]
+}
+
 variable "vpc_cidr_block" {}
 
 variable "public_subnets_cidr_blocks" {
@@ -8,6 +17,10 @@ variable "private_subnets_eks_cidr_blocks" {
  type = list(string) 
 }
 
+variable "private_subnets_rds_cidr_blocks" {
+ type = list(string) 
+}
+
 variable "tags" {
   type        = map(string)
   default     = {
@@ -15,3 +28,4 @@ variable "tags" {
     Environment = "dev"
   }
 }
+
