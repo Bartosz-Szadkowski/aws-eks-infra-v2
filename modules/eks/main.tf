5ac39544b1373ae3f51623b797a111b25e78f4a6
Bartosz-Szadkowski <bartoszszadkowski9@gmail.com>
2024-09-21 10:05:50 +0200

    modified the eks cluster sg

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -2,12 +2,14 @@
 resource "aws_eks_cluster" "this" {
   name     = "${var.tags["Environment"]}-eks-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
+  bootstrap_self_managed_addons = true
 
   vpc_config {
     subnet_ids              = var.subnet_ids
     endpoint_private_access = var.endpoint_private_access
     endpoint_public_access  = var.endpoint_public_access
     public_access_cidrs     = var.public_access_cidrs
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]
   }
 
   version = var.cluster_version
