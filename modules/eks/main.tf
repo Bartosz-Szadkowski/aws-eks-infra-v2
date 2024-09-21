39fc2d495ef69ada276d94e4d6b68d9c23bab0aa
Bartosz-Szadkowski <bartoszszadkowski9@gmail.com>
2024-09-21 12:44:18 +0200

    fixed typo

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -22,7 +22,7 @@ resource "aws_eks_cluster" "this" {
   }
 }
 data "aws_ssm_parameter" "eks_ami_release_version" {
-  name = "/aws/service/eks/optimized-ami/${aws_eks_cluster.example.version}/amazon-linux-2/recommended/release_version"
+  name = "/aws/service/eks/optimized-ami/${aws_eks_cluster.this.version}/amazon-linux-2/recommended/release_version"
 }
 
 resource "aws_eks_node_group" "eks_worker_nodes_group" {
