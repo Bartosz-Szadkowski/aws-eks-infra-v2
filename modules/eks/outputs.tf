beee0cc9625ac4096022a01ec0898753bfd149a7
Bartosz-Szadkowski <bartoszszadkowski9@gmail.com>
2024-09-21 14:10:53 +0200

    fixed eks output error

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -5,11 +5,3 @@ output "cluster_name" {
 output "cluster_endpoint" {
   value = aws_eks_cluster.this.endpoint
 }
-
-output "cluster_security_group_id" {
-  value = aws_security_group.eks_cluster_sg.id
-}
-
-output "worker_node_security_group_id" {
-  value = aws_security_group.worker_sg.id
-}
