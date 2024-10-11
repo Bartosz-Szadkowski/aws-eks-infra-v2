e406efee9669b5893cad55c61fcf4fd9cbebfe2b
Bartosz Szadkowski <bartoszszadkowski@Bartoszs-MacBook-Pro.local>
2024-10-11 19:24:04 +0200

    added policy for the secret

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -13,3 +13,23 @@ resource "aws_secretsmanager_secret_version" "argocd_secret_version" {
   secret_id     = aws_secretsmanager_secret.argocd_secret.id
   secret_string = random_password.argocd_password.result
 }
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_secretsmanager_secret_policy" "argocd_secret_policy" {
+  secret_arn = aws_secretsmanager_secret.argocd_secret.arn
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "secretsmanager:GetSecretValue"
+        Resource = aws_secretsmanager_secret.argocd_secret.arn
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.currnet.account_id}:user/${var.allowed_roles}"  # Replace with your IAM Role ARN
+        }
+      }
+    ]
+  })
+}
